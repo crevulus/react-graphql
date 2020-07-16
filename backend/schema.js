4ecfd76d8109ae36b92edd3d5961643a2e45ae56
crevulus <crevulus@gmail.com>
2020-07-16 16:13:34 +0200

    re-reconfig'd to accept both v3 & v4 of API; single launch query

--- backend/schema.js
+++ backend/schema.js
@@ -3,7 +3,6 @@ const {
   GraphQLInt,
   GraphQLString,
   GraphQLBoolean,
-  GraphQLScalarType,
   GraphQLList,
   GraphQLSchema,
 } = require("graphql");
@@ -11,6 +10,29 @@ const axios = require("axios");
 
 // Object type for returning launch data obj.
 const LaunchType = new GraphQLObjectType({
+  name: "LaunchV4",
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    rocket: { type: RocketType },
+  }),
+});
+
+// Need to create own object types if nested
+const RocketType = new GraphQLObjectType({
+  name: "Rocket",
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString },
+  }),
+});
+
+// Query for latest version of API
+const LaunchTypeV4 = new GraphQLObjectType({
   name: "Launch",
   fields: () => ({
     flight_number: { type: GraphQLInt },
@@ -23,8 +45,6 @@ const LaunchType = new GraphQLObjectType({
   }),
 });
 
-// NB: need to create own object types if nested
-
 const RootQuery = new GraphQLObjectType({
   name: "RootQuery",
   fields: {
@@ -32,7 +52,27 @@ const RootQuery = new GraphQLObjectType({
       type: new GraphQLList(LaunchType),
       resolve(parent, args) {
         return axios
+          .get("https://api.spacexdata.com/v3/launches")
+          .then((res) => res.data);
+      },
+    },
+    launchesV4: {
+      type: new GraphQLList(LaunchTypeV4),
+      resolve(parent, args) {
+        return axios
           .get("https://api.spacexdata.com/v4/launches")
+          .then((res) => res.data);
+      },
+    },
+    // get specific launch
+    launch: {
+      type: LaunchType, // single launch, so don't need list
+      args: {
+        flight_number: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
           .then((res) => res.data);
       },
     },
