462131f3075356fcd39f861d6088fa4b34702f95
crevulus <crevulus@gmail.com>
2020-07-17 20:33:57 +0200

    formatted single launch page

--- start/src/components/Launch.js
+++ start/src/components/Launch.js
@@ -1,8 +1,89 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+
+const LAUNCH_QUERY = gql`
+  query LaunchQuery($flight_number: Int!) {
+    launch(flight_number: $flight_number) {
+      flight_number
+      mission_name
+      launch_year
+      launch_success
+      rocket {
+        rocket_id
+        rocket_name
+        rocket_type
+      }
+    }
+  }
+`;
 
 export class Launch extends Component {
   render() {
-    return <div>Single Launch</div>;
+    let { flight_number } = this.props.match.params; // way to access params
+    flight_number = parseInt(flight_number);
+    return (
+      <Fragment>
+        {/* need to add variables when passing arguments to gql */}
+        <Query query={LAUNCH_QUERY} variables={{ flight_number }}>
+          {({ loading, error, data }) => {
+            if (loading) {
+              return <h4>Loading...</h4>;
+            }
+            if (error) {
+              console.log(error);
+            }
+            const {
+              flight_number,
+              mission_name,
+              launch_year,
+              launch_success,
+              rocket: { rocket_id, rocket_name, rocket_type },
+            } = data.launch;
+            return (
+              <div>
+                <h1 className="display-4 my-3">
+                  <span className="text-info">Mission: </span>
+                  {mission_name}
+                </h1>
+                <h4 className="mb-3">Launch Details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    Flight Number: {flight_number}
+                  </li>
+                  <li className="list-group-item">
+                    Launch Year: {launch_year}
+                  </li>
+                  <li className="list-group-item">
+                    Successful:{" "}
+                    <span
+                      className={classNames({
+                        "text-success": launch_success,
+                        "text-danger": !launch_success,
+                      })}
+                    >
+                      {launch_success ? "Yes" : "No"}
+                    </span>
+                  </li>
+                </ul>
+                <h4 className="my-3">Rocket Details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">Rocket ID: {rocket_id}</li>
+                  <li className="list-group-item">
+                    Rocket Name: {rocket_name}
+                  </li>
+                  <li className="list-group-item">
+                    Rocket Type: {rocket_type}
+                  </li>
+                </ul>
+              </div>
+            );
+          }}
+        </Query>
+      </Fragment>
+    );
   }
 }
 
